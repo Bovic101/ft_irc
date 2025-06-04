5d344c2e91ed845377c7d7f74612a06f0b981425
Bovic101 <Victorbolanle72@gmail.com>
2025-06-05 01:58:58 +0200

    error fixing encountered during execution

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,33 +1,41 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
-#include <iostream>
-#include <cstring>
-#include <set>
+
 #include <string>
+#include <set>
 
 class Client {
-public:
-	Client(); //added def constructor
-	Client(int fd);
-
-	~Client();
-
-	int getFd() const;
-	void appendBuffer(const std::string& data);
-	std::string &getBuffer();
-
-	void setNickname(const std::string& nickname);
-	const std::string& getNickname() const;
-	void addChannel(const std::string& channel);
-	void partChannel(const std::string& channel);
-	bool isInChannel(const std::string& channel) const;
-	const std::set<std::string>& getChannels() const;
-
-
 private:
-	int _fd;
-	std::string _buffer;
-	std::string _nickname;
-	std::set<std::string> _channels;
+    int _fd;
+    std::string _buffer;
+    std::string _nickname;
+    std::string _username; // added
+    std::set<std::string> _channels;
+    bool registeredChecker;
+    bool authenticatedChecker;
+
+public:
+    Client();
+    Client(int fd);
+    ~Client();
+
+    int getFd() const;
+    void appendBuffer(const std::string& data);
+    std::string& getBuffer();
+    void setNickname(const std::string& nickname);
+    const std::string& getNickname() const;
+
+    void addChannel(const std::string& channel);
+    void partChannel(const std::string& channel);
+    bool isInChannel(const std::string& channel) const;
+    const std::set<std::string>& getChannels() const;
+
+    //Added methods for registration and authentication
+    bool registeredCheckerFunc() const;
+    bool authenticatedCheckerFunc() const;
+    void grantAuth(bool status);
+    void setUsername(const std::string& username); // added
+    void markAsRegistered(bool status); 
 };
-#endif
+
+#endif // CLIENT_HPP
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,43 +1,67 @@
 #include "Client.hpp"
 
-Client::Client() : _fd(-1), _buffer(""), _nickname("") {}
+Client::Client()
+    : _fd(-1), _buffer(""), _nickname(""),
+      registeredChecker(false), authenticatedChecker(false) {}
 
-Client::Client(int fd) : _fd(fd), _buffer(""), _nickname("") {}
+Client::Client(int fd)
+    : _fd(fd), _buffer(""), _nickname(""),
+      registeredChecker(false), authenticatedChecker(false) {}
 
 Client::~Client() {}
 
 int Client::getFd() const {
-	return _fd;
+    return _fd;
 }
 
 void Client::appendBuffer(const std::string& data) {
-	_buffer += data;
+    _buffer += data;
 }
 
-std::string &Client::getBuffer() {
-	return _buffer;
+std::string& Client::getBuffer() {
+    return _buffer;
 }
 
 void Client::setNickname(const std::string& nickname) {
-	_nickname = nickname;
+    _nickname = nickname;
 }
 
 const std::string& Client::getNickname() const {
-	return _nickname;
+    return _nickname;
 }
 
 void Client::addChannel(const std::string& channel) {
-	_channels.insert(channel);
+    _channels.insert(channel);
 }
 
 void Client::partChannel(const std::string& channel) {
-	_channels.erase(channel);
+    _channels.erase(channel);
 }
 
 bool Client::isInChannel(const std::string& channel) const {
-	return _channels.find(channel) != _channels.end();
+    return _channels.find(channel) != _channels.end();
 }
 
 const std::set<std::string>& Client::getChannels() const {
-	return _channels;
+    return _channels;
+}
+
+bool Client::registeredCheckerFunc() const {
+    return registeredChecker;
+}
+
+bool Client::authenticatedCheckerFunc() const {
+    return authenticatedChecker;
+}
+
+void Client::grantAuth(bool status) {
+    authenticatedChecker = status;
+}
+
+void Client::setUsername(const std::string& username) {
+    _username = username;
+}
+
+void Client::markAsRegistered(bool status) {
+    registeredChecker = status;
 }
