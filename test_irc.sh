d5e815a12cee2ea50be147bed1d682ccc262bf6f
Bovic101 <Victorbolanle72@gmail.com>
2025-06-05 02:24:37 +0200

    update on my integration test source file

--- test_irc.sh
+++ test_irc.sh
@@ -15,7 +15,9 @@ run_client() {
   local name=$1
   echo -e "${INFO} ${YELLOW}Simulating client:${NC} $name"
   {
-    sleep 1
+    sleep 0.5
+    echo "PASS pass"
+    sleep 0.5
     echo "NICK $name"                  # Set nickname
     echo "USER $name 0 * :Real Name"   # Register user
     shift
@@ -28,54 +30,86 @@ run_client() {
 
 # User maryo sets up the channel and configures it
 run_client maryo \
-  "sleep 1.2" \
+  "sleep 1" \
   "echo 'JOIN #codingChannel'" \
   "sleep 1" \
   "echo 'TOPIC #codingChannel :Welcome to #codingChannel!'" \
   "sleep 1" \
   "echo 'MODE #codingChannel +i'" \
-  "sleep 0.9" \
+  "sleep 1" \
   "echo 'MODE #codingChannel +t'" \
-  "sleep 0.9" \
+  "sleep 1" \
   "echo 'MODE #codingChannel +k secret'" \
   "sleep 1" \
-  "echo 'MODE #codingChannel +l 5'" \
-  "sleep 0.8" \
+  "echo 'MODE #codingChannel +l 3'" \
+  "sleep 1" \
   "echo 'LIST'" \
-  "sleep 0.8" \
+  "sleep 1" \
   "echo 'INVITE mary #codingChannel'" \
+  "sleep 1" \
+  "echo 'INVITE victor #codingChannel'" \
+  "sleep 1" \
+  "echo 'INVITE blatifat #codingChannel'" \
   "sleep 1" \
   "echo 'QUIT :maryo out'" &
 
-# User mary joins after invite and sends a message
+# User mary joins and chats
 sleep 2
 run_client mary \
   "echo 'JOIN #codingChannel'" \
   "sleep 1" \
   "echo 'PRIVMSG #codingChannel :Hi everyone, mary here!'" \
   "sleep 1" \
-  "echo 'QUIT :mary says bye'" &
+  "echo 'PART #codingChannel'" \
+  "sleep 1" \
+  "echo 'JOIN #codingChannel'" \
+  "sleep 1" \
+  "echo 'QUIT :mary out again'" &
 
-# blatifat tries to join without invite (should fail)
+# blatifat joins using invite and sets topic
 sleep 3
 run_client blatifat \
   "echo 'JOIN #codingChannel'" \
   "sleep 1" \
-  "echo 'QUIT :blatifat failed to join'" &
+  "echo 'PRIVMSG #codingChannel :hello all!'" \
+  "sleep 1" \
+  "echo 'TOPIC #codingChannel :Updated topic by blatifat'" \
+  "sleep 1" \
+  "echo 'QUIT :done!'" &
 
-# victor joins using the key and is kicked
+# victor joins using key and is kicked
 sleep 4
 run_client victor \
   "echo 'JOIN #codingChannel secret'" \
   "sleep 1" \
-  "echo 'PRIVMSG #codingChannel :victor joined'" \
+  "echo 'PRIVMSG #codingChannel :I am victor!'" \
   "sleep 1" \
-  "echo 'KICK victor #codingChannel :You'\''re out'" \
+  "echo 'KICK blatifat #codingChannel :Bye bro'" \
   "sleep 1" \
-  "echo 'QUIT :victor kicked out'" &
+  "echo 'MODE #codingChannel -i'" \
+  "sleep 1" \
+  "echo 'QUIT :victor done'" &
 
-# List channels again
+# User test sends private message
 sleep 5
+run_client test \
+  "echo 'PRIVMSG victor :Private message test'" \
+  "sleep 1" \
+  "echo 'QUIT :test user done'" &
+
+# Error test cases: no PASS, no NICK, invalid JOIN
+sleep 6
+run_client errorCase \
+  "echo 'USER errorCase 0 * :Should fail'" \
+  "sleep 1" \
+  "echo 'JOIN notAChannel'" \
+  "sleep 1" \
+  "echo 'JOIN #codingChannel wrongKey'" \
+  "sleep 1" \
+  "echo 'QUIT :error testing done'" &
+
+# Final LIST after all ops
+sleep 7
 run_client list_checker \
   "echo 'LIST'" \
   "sleep 1" \
@@ -83,4 +117,5 @@ run_client list_checker \
 
 # Wait for all client simulations to finish
 wait
-echo -e "${CHECK} ${GREEN}All IRC command tests are fully completed. Congrats!${NC}"
+echo -e "${CHECK} ${GREEN}All ft_irc mandatory command edge cases tested successfully.${NC}"
+
